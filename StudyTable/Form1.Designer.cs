50a2989d087efa69238895f8960043ffcf0d9c27
Cha Juyeon <ch4102109@gmail.com>
2023-12-03 11:39:49 +0900

    "third commit"
    폼변경창 만듬
    뭔가 적은것 같아서 테마별로 색과 이미지 바뀌는 부분 추가
    추가로 나가기 버튼 만듬

--- StudyTable/Form1.Designer.cs
+++ StudyTable/Form1.Designer.cs
@@ -28,13 +28,12 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.game2 = new System.Windows.Forms.Button();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
-            this.apply = new System.Windows.Forms.Button();
             this.Colorlabel = new System.Windows.Forms.Label();
             this.Fontlabel = new System.Windows.Forms.Label();
-            this.Themalabel = new System.Windows.Forms.Label();
             this.FontChange = new System.Windows.Forms.Button();
             this.BackgroundChange = new System.Windows.Forms.Button();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
@@ -46,12 +45,21 @@
             this.calender = new System.Windows.Forms.Button();
             this.calcul = new System.Windows.Forms.Button();
             this.game1 = new System.Windows.Forms.Button();
-            this.logout = new System.Windows.Forms.Button();
             this.memo = new System.Windows.Forms.Button();
             this.watch = new System.Windows.Forms.Button();
             this.label3 = new System.Windows.Forms.Label();
             this.BackColorlabel = new System.Windows.Forms.Label();
-            this.ColorBt = new System.Windows.Forms.Button();
+            this.logout = new System.Windows.Forms.Button();
+            this.close = new System.Windows.Forms.Button();
+            this.fontDialog1 = new System.Windows.Forms.FontDialog();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.apply = new System.Windows.Forms.Button();
+            this.domainUpDown1 = new System.Windows.Forms.DomainUpDown();
+            this.Themalabel = new System.Windows.Forms.Label();
+            this.imageListWatch = new System.Windows.Forms.ImageList(this.components);
+            this.imageListCalendar = new System.Windows.Forms.ImageList(this.components);
+            this.imageListMemo = new System.Windows.Forms.ImageList(this.components);
+            this.imageListCalculator = new System.Windows.Forms.ImageList(this.components);
             this.groupBox2.SuspendLayout();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
@@ -69,11 +77,11 @@
             // 
             // groupBox2
             // 
-            this.groupBox2.Controls.Add(this.ColorBt);
+            this.groupBox2.Controls.Add(this.Themalabel);
             this.groupBox2.Controls.Add(this.apply);
+            this.groupBox2.Controls.Add(this.domainUpDown1);
             this.groupBox2.Controls.Add(this.Colorlabel);
             this.groupBox2.Controls.Add(this.Fontlabel);
-            this.groupBox2.Controls.Add(this.Themalabel);
             this.groupBox2.Controls.Add(this.FontChange);
             this.groupBox2.Controls.Add(this.BackgroundChange);
             this.groupBox2.Font = new System.Drawing.Font("굴림", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
@@ -84,26 +92,15 @@
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "폼 변경";
             // 
-            // apply
-            // 
-            this.apply.FlatAppearance.BorderSize = 0;
-            this.apply.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.apply.Location = new System.Drawing.Point(214, 111);
-            this.apply.Name = "apply";
-            this.apply.Size = new System.Drawing.Size(0, 0);
-            this.apply.TabIndex = 12;
-            this.apply.Text = "▣";
-            this.apply.UseVisualStyleBackColor = true;
-            // 
             // Colorlabel
             // 
             this.Colorlabel.AutoSize = true;
             this.Colorlabel.Font = new System.Drawing.Font("굴림", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.Colorlabel.Location = new System.Drawing.Point(52, 80);
+            this.Colorlabel.Location = new System.Drawing.Point(34, 80);
             this.Colorlabel.Name = "Colorlabel";
-            this.Colorlabel.Size = new System.Drawing.Size(38, 16);
+            this.Colorlabel.Size = new System.Drawing.Size(56, 16);
             this.Colorlabel.TabIndex = 9;
-            this.Colorlabel.Text = "BC :";
+            this.Colorlabel.Text = "Color :";
             // 
             // Fontlabel
             // 
@@ -115,16 +112,6 @@
             this.Fontlabel.TabIndex = 5;
             this.Fontlabel.Text = "Font :";
             // 
-            // Themalabel
-            // 
-            this.Themalabel.AutoSize = true;
-            this.Themalabel.Font = new System.Drawing.Font("굴림", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.Themalabel.Location = new System.Drawing.Point(34, 114);
-            this.Themalabel.Name = "Themalabel";
-            this.Themalabel.Size = new System.Drawing.Size(56, 16);
-            this.Themalabel.TabIndex = 6;
-            this.Themalabel.Text = "Color :";
-            // 
             // FontChange
             // 
             this.FontChange.FlatAppearance.BorderColor = System.Drawing.Color.Gainsboro;
@@ -135,6 +122,7 @@
             this.FontChange.TabIndex = 7;
             this.FontChange.Text = "폰트변경";
             this.FontChange.UseVisualStyleBackColor = true;
+            this.FontChange.Click += new System.EventHandler(this.FontChange_Click);
             // 
             // BackgroundChange
             // 
@@ -146,6 +134,7 @@
             this.BackgroundChange.TabIndex = 8;
             this.BackgroundChange.Text = "배경색 변경";
             this.BackgroundChange.UseVisualStyleBackColor = true;
+            this.BackgroundChange.Click += new System.EventHandler(this.BackgroundChange_Click);
             // 
             // groupBox1
             // 
@@ -225,7 +214,7 @@
             // 
             this.calender.FlatAppearance.BorderSize = 0;
             this.calender.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.calender.Image = ((System.Drawing.Image)(resources.GetObject("calender.Image")));
+            this.calender.ImageList = this.imageListCalendar;
             this.calender.Location = new System.Drawing.Point(103, 253);
             this.calender.Name = "calender";
             this.calender.Size = new System.Drawing.Size(120, 120);
@@ -236,7 +225,7 @@
             // 
             this.calcul.FlatAppearance.BorderSize = 0;
             this.calcul.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.calcul.Image = ((System.Drawing.Image)(resources.GetObject("calcul.Image")));
+            this.calcul.ImageList = this.imageListCalculator;
             this.calcul.Location = new System.Drawing.Point(263, 253);
             this.calcul.Name = "calcul";
             this.calcul.Size = new System.Drawing.Size(120, 120);
@@ -254,24 +243,11 @@
             this.game1.Text = "게임";
             this.game1.UseVisualStyleBackColor = true;
             // 
-            // logout
-            // 
-            this.logout.FlatAppearance.BorderColor = System.Drawing.Color.Gainsboro;
-            this.logout.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.logout.Font = new System.Drawing.Font("굴림", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.logout.Location = new System.Drawing.Point(841, 50);
-            this.logout.Name = "logout";
-            this.logout.Size = new System.Drawing.Size(99, 29);
-            this.logout.TabIndex = 45;
-            this.logout.Text = "로그아웃";
-            this.logout.UseVisualStyleBackColor = true;
-            this.logout.Click += new System.EventHandler(this.logout_Click);
-            // 
             // memo
             // 
             this.memo.FlatAppearance.BorderSize = 0;
             this.memo.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.memo.Image = ((System.Drawing.Image)(resources.GetObject("memo.Image")));
+            this.memo.ImageList = this.imageListMemo;
             this.memo.Location = new System.Drawing.Point(193, 108);
             this.memo.Name = "memo";
             this.memo.Size = new System.Drawing.Size(120, 120);
@@ -282,7 +258,7 @@
             // 
             this.watch.FlatAppearance.BorderSize = 0;
             this.watch.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.watch.Image = ((System.Drawing.Image)(resources.GetObject("watch.Image")));
+            this.watch.ImageList = this.imageListWatch;
             this.watch.Location = new System.Drawing.Point(33, 108);
             this.watch.Name = "watch";
             this.watch.Size = new System.Drawing.Size(120, 120);
@@ -308,22 +284,110 @@
             this.BackColorlabel.TabIndex = 55;
             this.BackColorlabel.Text = "\r\n\r\n\r\n\r\n\r\n";
             // 
-            // ColorBt
+            // logout
             // 
-            this.ColorBt.FlatAppearance.BorderColor = System.Drawing.Color.Gainsboro;
-            this.ColorBt.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.ColorBt.Location = new System.Drawing.Point(114, 114);
-            this.ColorBt.Name = "ColorBt";
-            this.ColorBt.Size = new System.Drawing.Size(130, 30);
-            this.ColorBt.TabIndex = 13;
-            this.ColorBt.Text = "색 변경";
-            this.ColorBt.UseVisualStyleBackColor = true;
+            this.logout.FlatAppearance.BorderColor = System.Drawing.Color.Gainsboro;
+            this.logout.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.logout.Font = new System.Drawing.Font("굴림", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.logout.Location = new System.Drawing.Point(785, 37);
+            this.logout.Name = "logout";
+            this.logout.Size = new System.Drawing.Size(99, 29);
+            this.logout.TabIndex = 45;
+            this.logout.Text = "로그아웃";
+            this.logout.UseVisualStyleBackColor = true;
+            this.logout.Click += new System.EventHandler(this.logout_Click);
+            // 
+            // close
+            // 
+            this.close.FlatAppearance.BorderSize = 0;
+            this.close.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.close.Font = new System.Drawing.Font("굴림", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.close.Image = ((System.Drawing.Image)(resources.GetObject("close.Image")));
+            this.close.Location = new System.Drawing.Point(905, 26);
+            this.close.Name = "close";
+            this.close.Size = new System.Drawing.Size(50, 50);
+            this.close.TabIndex = 56;
+            this.close.UseVisualStyleBackColor = true;
+            this.close.Click += new System.EventHandler(this.close_Click);
+            // 
+            // apply
+            // 
+            this.apply.FlatAppearance.BorderSize = 0;
+            this.apply.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.apply.Location = new System.Drawing.Point(214, 112);
+            this.apply.Name = "apply";
+            this.apply.Size = new System.Drawing.Size(30, 30);
+            this.apply.TabIndex = 14;
+            this.apply.Text = "▣";
+            this.apply.UseVisualStyleBackColor = true;
+            this.apply.Click += new System.EventHandler(this.apply_Click);
+            // 
+            // domainUpDown1
+            // 
+            this.domainUpDown1.Items.Add("RED");
+            this.domainUpDown1.Items.Add("BLUE");
+            this.domainUpDown1.Items.Add("YELLOW");
+            this.domainUpDown1.Items.Add("ORIGINAL");
+            this.domainUpDown1.Location = new System.Drawing.Point(114, 114);
+            this.domainUpDown1.Name = "domainUpDown1";
+            this.domainUpDown1.ReadOnly = true;
+            this.domainUpDown1.Size = new System.Drawing.Size(94, 26);
+            this.domainUpDown1.TabIndex = 13;
+            this.domainUpDown1.Text = "테마 변경";
+            this.domainUpDown1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Themalabel
+            // 
+            this.Themalabel.AutoSize = true;
+            this.Themalabel.Font = new System.Drawing.Font("굴림", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.Themalabel.Location = new System.Drawing.Point(24, 119);
+            this.Themalabel.Name = "Themalabel";
+            this.Themalabel.Size = new System.Drawing.Size(66, 16);
+            this.Themalabel.TabIndex = 15;
+            this.Themalabel.Text = "Thema :";
+            // 
+            // imageListWatch
+            // 
+            this.imageListWatch.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageListWatch.ImageStream")));
+            this.imageListWatch.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageListWatch.Images.SetKeyName(0, "O_watch.png");
+            this.imageListWatch.Images.SetKeyName(1, "R_watch.png");
+            this.imageListWatch.Images.SetKeyName(2, "B_watch.png");
+            this.imageListWatch.Images.SetKeyName(3, "Y_watch.png");
+            // 
+            // imageListCalendar
+            // 
+            this.imageListCalendar.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageListCalendar.ImageStream")));
+            this.imageListCalendar.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageListCalendar.Images.SetKeyName(0, "O_calend.png");
+            this.imageListCalendar.Images.SetKeyName(1, "R_calend.png");
+            this.imageListCalendar.Images.SetKeyName(2, "B_calend.png");
+            this.imageListCalendar.Images.SetKeyName(3, "Y_calend.png");
+            // 
+            // imageListMemo
+            // 
+            this.imageListMemo.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageListMemo.ImageStream")));
+            this.imageListMemo.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageListMemo.Images.SetKeyName(0, "O_memo.png");
+            this.imageListMemo.Images.SetKeyName(1, "R_memo.png");
+            this.imageListMemo.Images.SetKeyName(2, "B_memo.png");
+            this.imageListMemo.Images.SetKeyName(3, "Y_memo.png");
+            // 
+            // imageListCalculator
+            // 
+            this.imageListCalculator.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageListCalculator.ImageStream")));
+            this.imageListCalculator.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageListCalculator.Images.SetKeyName(0, "O_calcul.png");
+            this.imageListCalculator.Images.SetKeyName(1, "R_calcul.png");
+            this.imageListCalculator.Images.SetKeyName(2, "B_calcul.png");
+            this.imageListCalculator.Images.SetKeyName(3, "Y_calcul.png");
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(984, 561);
+            this.Controls.Add(this.close);
             this.Controls.Add(this.game2);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
@@ -350,10 +414,8 @@
         #endregion
         private System.Windows.Forms.Button game2;
         private System.Windows.Forms.GroupBox groupBox2;
-        private System.Windows.Forms.Button apply;
         private System.Windows.Forms.Label Colorlabel;
         private System.Windows.Forms.Label Fontlabel;
-        private System.Windows.Forms.Label Themalabel;
         private System.Windows.Forms.Button FontChange;
         private System.Windows.Forms.Button BackgroundChange;
         private System.Windows.Forms.GroupBox groupBox1;
@@ -365,12 +427,21 @@
         private System.Windows.Forms.Button calender;
         private System.Windows.Forms.Button calcul;
         private System.Windows.Forms.Button game1;
-        private System.Windows.Forms.Button logout;
         private System.Windows.Forms.Button memo;
         private System.Windows.Forms.Button watch;
         public System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label BackColorlabel;
-        private System.Windows.Forms.Button ColorBt;
+        private System.Windows.Forms.Label Themalabel;
+        private System.Windows.Forms.Button apply;
+        private System.Windows.Forms.Button logout;
+        private System.Windows.Forms.Button close;
+        private System.Windows.Forms.FontDialog fontDialog1;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.ImageList imageListWatch;
+        private System.Windows.Forms.ImageList imageListCalendar;
+        private System.Windows.Forms.ImageList imageListMemo;
+        private System.Windows.Forms.ImageList imageListCalculator;
+        private System.Windows.Forms.DomainUpDown domainUpDown1;
     }
 }
 
--- StudyTable/Form1.cs
+++ StudyTable/Form1.cs
@@ -31,6 +31,11 @@ namespace StudyTable
             Form2 _Form2 = new Form2(this);
             _Form2.ShowDialog();
             if (!m_blLoginCheck) this.Close();
+
+            watch.Image = imageListWatch.Images[0];
+            memo.Image = imageListMemo.Images[0];
+            calender.Image = imageListCalendar.Images[0];
+            calcul.Image = imageListCalculator.Images[0];
         }
     
 
@@ -56,10 +61,172 @@ namespace StudyTable
 
         private void logout_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("로그아웃하였습니다.\n로그인화면으로 전환합니다.");
+            MessageBox.Show("         로그아웃하였습니다.\n     로그인화면으로 전환합니다.");
 
             Form2 _Form2 = new Form2(this);
             _Form2.ShowDialog();
+        }
+
+        private void close_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("    프로그램을 종료합니다.");
+            Application.Exit();
+        }
+
+        private void FontChange_Click(object sender, EventArgs e)
+        {
+            if (fontDialog1.ShowDialog() == DialogResult.OK)     //변수 없이 확인버튼을 눌렀을 때만 폰트변경
+            {
+                logout.Font = fontDialog1.Font;
+                logout.ForeColor = fontDialog1.Color;
+                game1.Font = fontDialog1.Font;
+                game1.ForeColor = fontDialog1.Color;
+                game2.Font = fontDialog1.Font;
+                game2.ForeColor = fontDialog1.Color;
+                groupBox1.Font = fontDialog1.Font;
+                groupBox1.ForeColor = fontDialog1.Color;
+                groupBox2.Font = fontDialog1.Font;
+                groupBox2.ForeColor = fontDialog1.Color;
+                Fontlabel.Font = fontDialog1.Font;
+                Fontlabel.ForeColor = fontDialog1.Color;
+                Colorlabel.Font = fontDialog1.Font;
+                Colorlabel.ForeColor = fontDialog1.Color;
+                Themalabel.Font = fontDialog1.Font;
+                Themalabel.ForeColor = fontDialog1.Color;
+                FontChange.Font = fontDialog1.Font;
+                FontChange.ForeColor = fontDialog1.Color;
+                BackgroundChange.Font = fontDialog1.Font;
+                BackgroundChange.ForeColor = fontDialog1.Color;
+                domainUpDown1.Font = fontDialog1.Font;
+                domainUpDown1.ForeColor = fontDialog1.Color;
+                apply.Font = fontDialog1.Font;
+                apply.ForeColor = fontDialog1.Color;
+                game1.BackColor = colorDialog1.Color;
+                game2.BackColor = colorDialog1.Color;
+            }
+        }
+
+        private void BackgroundChange_Click(object sender, EventArgs e)
+        {
+            if (colorDialog1.ShowDialog() == DialogResult.OK)                      //변수를 통하여 해당 변수의 값이 확인버튼일 때만 색상 변경
+            {
+                BackColorlabel.BackColor = colorDialog1.Color;
+                label1.BackColor = colorDialog1.Color;
+                label3.BackColor = colorDialog1.Color;
+                groupBox1.BackColor = colorDialog1.Color;
+                groupBox2.BackColor = colorDialog1.Color;
+                close.BackColor = colorDialog1.Color;
+                logout.BackColor = colorDialog1.Color;
+                watch.BackColor = colorDialog1.Color;
+                calcul.BackColor = colorDialog1.Color;
+                calender.BackColor = colorDialog1.Color;
+                memo.BackColor = colorDialog1.Color;
+                domainUpDown1.BackColor = colorDialog1.Color;
+            }
+        }
+
+        private void apply_Click(object sender, EventArgs e)
+        {
+            if (domainUpDown1.SelectedItem == "RED")
+            {
+                BackColorlabel.BackColor = Color.LightPink; 
+                label1.ForeColor = Color.Crimson;
+
+                label1.BackColor = Color.LightPink;
+                label3.BackColor = Color.LightPink;
+                groupBox1.BackColor = Color.LightPink;
+                groupBox2.BackColor = Color.LightPink;
+                close.BackColor = Color.LightPink;
+                logout.BackColor = Color.LightPink;
+                domainUpDown1.BackColor = Color.LightPink;
+                game1.BackColor = Color.LightPink;
+                game2.BackColor = Color.LightPink;
+
+                watch.BackColor = Color.LightPink;
+                calcul.BackColor = Color.LightPink;
+                calender.BackColor = Color.LightPink;
+                memo.BackColor = Color.LightPink;
+
+                watch.Image = imageListWatch.Images[1];
+                memo.Image = imageListMemo.Images[1];
+                calender.Image = imageListCalendar.Images[1];
+                calcul.Image = imageListCalculator.Images[1];
+            }
+            else if (domainUpDown1.SelectedItem == "BLUE")
+            {
+                BackColorlabel.BackColor = Color.LightSkyBlue;
+                label1.ForeColor = Color.DarkBlue;
+
+                label1.BackColor = Color.LightSkyBlue;
+                label3.BackColor = Color.LightSkyBlue;
+                groupBox1.BackColor = Color.LightSkyBlue;
+                groupBox2.BackColor = Color.LightSkyBlue;
+                close.BackColor = Color.LightSkyBlue;
+                logout.BackColor = Color.LightSkyBlue;
+                domainUpDown1.BackColor = Color.LightSkyBlue;
+                game1.BackColor = Color.LightSkyBlue;
+                game2.BackColor = Color.LightSkyBlue;
+
+                watch.BackColor = Color.LightSkyBlue;
+                calcul.BackColor = Color.LightSkyBlue;
+                calender.BackColor = Color.LightSkyBlue;
+                memo.BackColor = Color.LightSkyBlue;
+
+                watch.Image = imageListWatch.Images[2];
+                memo.Image = imageListMemo.Images[2];
+                calender.Image = imageListCalendar.Images[2];
+                calcul.Image = imageListCalculator.Images[2];
+            }
+            else if (domainUpDown1.SelectedItem == "YELLOW")
+            {
+                BackColorlabel.BackColor = Color.LightYellow;
+                label1.ForeColor = Color.DarkGoldenrod;
+
+                label1.BackColor = Color.LightYellow;
+                label3.BackColor = Color.LightYellow;
+                groupBox1.BackColor = Color.LightYellow;
+                groupBox2.BackColor = Color.LightYellow;
+                close.BackColor = Color.LightYellow;
+                logout.BackColor = Color.LightYellow;
+                domainUpDown1.BackColor = Color.LightYellow;
+                game1.BackColor = Color.LightYellow;
+                game2.BackColor = Color.LightYellow;
+
+                watch.BackColor = Color.LightYellow;
+                calcul.BackColor = Color.LightYellow;
+                calender.BackColor = Color.LightYellow;
+                memo.BackColor = Color.LightYellow;
+
+                watch.Image = imageListWatch.Images[3];
+                memo.Image = imageListMemo.Images[3];
+                calender.Image = imageListCalendar.Images[3];
+                calcul.Image = imageListCalculator.Images[3];
+            }
+            else
+            {
+                BackColorlabel.BackColor = SystemColors.Control;
+                label1.ForeColor = Color.Black;
+
+                label1.BackColor = SystemColors.Control;
+                label3.BackColor = SystemColors.Control;
+                groupBox1.BackColor = SystemColors.Control;
+                groupBox2.BackColor = SystemColors.Control;
+                close.BackColor = SystemColors.Control;
+                logout.BackColor = SystemColors.Control;
+                domainUpDown1.BackColor = SystemColors.Control;
+                game1.BackColor = SystemColors.Control;
+                game2.BackColor = SystemColors.Control;
+
+                watch.BackColor = SystemColors.Control;
+                calcul.BackColor = SystemColors.Control;
+                calender.BackColor = SystemColors.Control;
+                memo.BackColor = SystemColors.Control;
+
+                watch.Image = imageListWatch.Images[0];
+                memo.Image = imageListMemo.Images[0];
+                calender.Image = imageListCalendar.Images[0];
+                calcul.Image = imageListCalculator.Images[0];
+            }
         }
     }
 }
