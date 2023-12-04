a085f1bf79aabc5c1c46d7c08e63f923f9f4f86c
sosso00 <152760705+sosso00@users.noreply.github.com>
2023-12-04 21:37:47 +0900

    add memo directory
    선배가 만든 메모장 기능 폼 보냄...
    그런데 메세지 창만 떠서 다시 만들어야하니까 간단하게만 합쳐줘~

---
+++ 메모장/MEMO/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace MEMO
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void 새파일NToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(새파일ToolStripMenuItem.Text);
+        }
+
+        private void 열기OToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(열기ToolStripMenuItem.Text);
+        }
+
+        private void 닫기CToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(닫기ToolStripMenuItem.Text);
+        }
+
+        private void 저장SToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(저장ToolStripMenuItem.Text);
+        }
+
+        private void 다른이름으로저장AToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(다른이름으로저장ToolStripMenuItem.Text);
+        }
+
+        private void 인쇄PToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(인쇄ToolStripMenuItem.Text);
+        }
+
+        private void 미리보기VToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(미리보기ToolStripMenuItem.Text);
+        }
+
+        private void 종료XToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+           
+        }
+
+        private void 잘라내기TToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(잘라내기TToolStripMenuItem.Text);
+        }
+
+        private void 복사CToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(복사CToolStripMenuItem.Text);
+        }
+
+        private void 붙여놓기PToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(붙여놓기PToolStripMenuItem.Text);
+        }
+
+        private void 프로그램정보AToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(프로그램정보AToolStripMenuItem.Text);
+        }
+
+        private void 실행취소UToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(실행취소UToolStripMenuItem.Text);
+        }
+
+        private void 잘라내기TToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(잘라내기TToolStripMenuItem1.Text);
+        }
+
+        private void 복사CToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(복사CToolStripMenuItem1.Text);
+        }
+
+        private void 삭제DToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(삭제DToolStripMenuItem.Text);
+        }
+
+        private void 모두선택AToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(모두선택AToolStripMenuItem.Text);
+        }
+    }
+}
